917d4e3afdc8fb2be5cb840209833126d6fd62c5
Tatarenko Boris <fourhtyoz@gmail.com>
2025-01-13 17:26:13 +0300

    webRTC streaming init

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const http = require('http');
+const WebSocket = require('ws');
+
+const app = express();
+
+const server = http.createServer(app);
+
+const wss = new WebSocket.Server({ server });
+
+app.use(express.static('public'));
+
+wss.on('connection', (ws) => {
+    ws.on('message', (message) => {
+        wss.clients.forEach((client) => {
+            if (client !== ws && client.readyState === WebSocket.OPEN) {
+                client.send(message);
+            }
+        });
+    });
+
+    ws.send('Connected to WebSocket server');
+});
+
+server.listen(8000, () => {
+    console.log('Server running on http://localhost:8000');
+});
